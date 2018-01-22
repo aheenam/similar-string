17034324cb0e0c43b5a8789ff0e5fc654ca27f31
Rathes Sachchithananthan <rathes@aheenam.com>
2018-01-22 23:18:57 +0000

    Apply fixes from StyleCI

--- src/SimilarString.php
+++ src/SimilarString.php
@@ -1,31 +1,34 @@
 <?php
+
 namespace Aheenam\SimilarString;
 
 class SimilarString
 {
     /**
      * returns a collection of the words in $collection ordered
-     * by their similarity to $word
+     * by their similarity to $word.
      *
-     * @param string $word
+     * @param string                         $word
      * @param \Illuminate\Support\Collection $collection
+     *
      * @return void
      */
     public function orderBySimilarity($word, $collection)
     {
         return collect($collection)
             ->sortByDesc(function ($item) use ($word) {
-                return (new Dice)($word, $item);
+                return (new Dice())($word, $item);
             })
             ->values()
             ->all();
     }
 
     /**
-     * returns the best match of a given collection
+     * returns the best match of a given collection.
      *
-     * @param string $word
+     * @param string                         $word
      * @param \Illuminate\Support\Collection $collection
+     *
      * @return string
      */
     public function findMostSimilar($word, $collection)
